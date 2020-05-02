8e5339a8e5e2a5886f70432da594bb4ec6c37050
Tycho van der Ouderaa <tychovdo@gmail.com>
2020-05-02 19:00:57 +0200

    Update constants.py

--- hyperkite/constants.py
+++ hyperkite/constants.py
@@ -1,4 +1,4 @@
-# API_URL = 'https://hyperkite.ai/api/'
-API_URL = 'http://localhost:5000/api/'
+API_URL = 'https://hyperkite.ai/api/'
+# API_URL = 'http://localhost:5000/api/'
 
 TEST_STUDY_KEY = ''
